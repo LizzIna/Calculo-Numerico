7ed5889ae98087c6827d72bd18b9b43ea0fa7bf4
LizzIna <valentinacalderon466@gmail.com>
2026-09-03 14:36:39 -0400

    Fibonacci division numeros consecutivos

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,4 +1,4 @@
-n = int(input("Ingresa la cantidad de numeros: "))
+n = int(input("ingresa la cantidad de numeros: "))
 
 a = 0
 b = 1
@@ -7,6 +7,11 @@ fibonacci = []
 
 for i in range(n):
     fibonacci.append(a)
+    
+    if a != 0:
+        division = b / a
+        print(f"Paso {i}: {b} / {a} = {division:.3f}")
+    
     a, b = b, a + b
 
 print("Serie de Fibonacci completa: ", fibonacci)
